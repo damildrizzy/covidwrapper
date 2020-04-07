a41b0f9887b2f9cf2fda0fa5c74cd73ff77cd804
Samuel Adekoya <damiloladrizzy@gmail.com>
2020-04-07 10:07:26 +0100

    added tests for all_data

--- tests/test_all_data.py
+++ tests/test_all_data.py
@@ -1,8 +1,8 @@
 from covidwrapper import Covid
 
 def test_all():
-    # response = Covid.all_data()
+    response = Covid.all_data()
 
-    # assert isinstance(response, list)
-    # assert isinstance(response[0], dict)
-    pass
+    assert isinstance(response, list)
+    assert isinstance(response[0], dict)
+    
